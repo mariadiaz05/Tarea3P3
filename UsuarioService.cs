2e5c2474c739e28456b76a4eb1f283928a59e4cd
mariadiaz05 <mariadiazcampana0805@gmail.com>
2025-08-01 01:59:14 -0400

    UsuarioService.cs

--- UsuarioService.cs
+++ UsuarioService.cs
@@ -3,11 +3,11 @@ public void EditarUsuario(int id, string nuevoNombre, string nuevoCorreo)
     var usuario = usuarios.FirstOrDefault(u => u.Id == id);
     if (usuario == null)
     {
-        Console.WriteLine("❌ Usuario no encontrado.");
+        Console.WriteLine(" Usuario no encontrado.");
         return;
     }
 
     usuario.Nombre = nuevoNombre;
     usuario.Correo = nuevoCorreo;
-    Console.WriteLine("✔️ Usuario actualizado correctamente.");
+    Console.WriteLine(" Usuario actualizado correctamente.");
 }
